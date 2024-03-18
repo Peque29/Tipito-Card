e8cf6594dbafe9314b54e2929094f0c83da15932
Peque29 <lichireale@gmail.com>
2024-03-18 16:49:53 -0300

    Solved TipitoCard center problem

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ function App() {
 
   return (
     <>
-      <div class="bg-black rounded-md shadow-2xl grid justify-items-center p-8">  
-        <h1 class="text-lg pb-5 font-bold">Tipito Enojado</h1>
-        <TipitoCard/>
-        <Redes/>
+      <div class="grid justify-items-center h-min ">
+        <div class="bg-black rounded-md shadow-2xl grid justify-items-center p-8 mt-20">  
+          <h1 class="text-lg pb-5 font-bold">Tipito Enojado</h1>
+          <TipitoCard/>
+          <Redes/>
+        </div>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div class="rounded grid">
+    <div class="rounded grid w-screen h-screen">
       <NavBar />
       <App />
     </div>
