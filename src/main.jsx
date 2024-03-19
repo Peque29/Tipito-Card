6fa212903685bb9f4e9df23ac3105804f1a6c69a
Peque29 <lichireale@gmail.com>
2024-03-19 14:19:07 -0300

    Added NavBar and fixed some errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import NavBar from '/src/components/NavBar'
+import Footer from '/src/components/Footer'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div class="rounded grid w-screen h-screen">
       <NavBar />
       <App />
+      <Footer />
     </div>
   </React.StrictMode>,
 )
